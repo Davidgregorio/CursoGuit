31743d7d1d9766cc89b879df258468073d0438fc
David de Melo <dhavidy@gmail.com>
2019-06-13 19:58:04 -0300

    Add Sub

--- projeto.c
+++ projeto.c
@@ -3,7 +3,14 @@ int soma(int a, int b){
 	int soma_num = a+b;
 	return soma_num;	
 }
+
+int subtracao(int a, int b){
+		int sub = a+b;
+	return sub;	
+}
+
 int main(){
 	printf("Soma dos dois números: %d\n",soma (5,5));
+		printf("Subtracao dos dois números: %d\n",subtracao (5,5));
 	return 0;
 }
